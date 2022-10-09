157026387c0b65bc5ccb680a3c64166b9690df49
mushka <nikitamushka@mail.ru>
2022-10-09 15:41:27 +0300

    updated default create-react-app files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Provider as MobxProvider } from "mobx-react";
+import "./App.css";
+
+import store from "./stores/index";
+
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import SignIn from "./pages/LoginPage/SignIn";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
+  fetch("http://localhost:8080/api/user").then(function (response) {
+    console.log(response);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MobxProvider {...store}>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUpPage />} />
+
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </MobxProvider>
     </div>
   );
 }
