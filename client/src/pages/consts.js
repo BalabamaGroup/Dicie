5c7b8fee40c7d70a3dbfe05338bce06f859c5096
mushka <nikitamushka@mail.ru>
2022-10-09 15:48:32 +0300

    created default consts file

---
+++ client/src/pages/consts.js
@@ -0,0 +1,12 @@
+export const ROUTES = Object.freeze({
+  HOME: "/",
+  ABOUT: "about/",
+  CONTACT: "/contact",
+  SIGN_IN: "/auth/signin",
+  SIGN_UP: "/auth/signup",
+});
+
+export const ROLE_TYPES = Object.freeze({
+  USER: "ROLE_USER",
+  ADMIN: "ROLE_ADMIN",
+});
