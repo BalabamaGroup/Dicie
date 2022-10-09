9e9149819beb4f4f0e87fcf64d7366a5c217530d
mushka <nikitamushka@mail.ru>
2022-10-09 15:43:51 +0300

    created ValidationInput index file

---
+++ client/src/components/ValidationInput/index.ts
@@ -0,0 +1,11 @@
+import UsernameInput from "./UsernameInput";
+import EmailInput from "./EmailInput";
+import PasswordInput from "./PasswordInput";
+
+const ValidationInput = {
+  UsernameInput,
+  EmailInput,
+  PasswordInput,
+};
+
+export default ValidationInput;
