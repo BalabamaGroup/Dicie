ed760b3aef24b537c61e006da76f544e4e49260f
mushka <nikitamushka@mail.ru>
2022-10-23 18:09:20 +0300

    udpated App Routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes as RoutesRRD, Route, Navigate } from "react-router-dom";
 import { Provider as MobxProvider } from "mobx-react";
 import "./App.css";
 
@@ -10,23 +10,28 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
-function App() {
+import Routes from "./constants/routes";
+import Room from "./pages/Room";
+
+const App = () => {
   return (
     <div className="App">
       <MobxProvider {...store}>
-        <Routes>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
+        <RoutesRRD>
+          <Route path={Routes.SIGN_IN} element={<SignIn />} />
+          <Route path={Routes.SIGN_UP} element={<SignUp />} />
 
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path={Routes.HOME} element={<Home />} />
+          <Route path={Routes.ABOUT} element={<About />} />
+          <Route path={Routes.CONTACT} element={<Contact />} />
 
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+          <Route path={Routes.ROOM} element={<Room />} />
+
+          <Route path="*" element={<Navigate to={Routes.HOME} replace />} />
+        </RoutesRRD>
       </MobxProvider>
     </div>
   );
-}
+};
 
 export default App;
