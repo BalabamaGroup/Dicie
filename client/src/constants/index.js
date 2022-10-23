7c51603725389a363b71c7d66cd652844ab28226
mushka <nikitamushka@mail.ru>
2022-10-23 18:10:40 +0300

    created constants for protocols and hosts

---
+++ client/src/constants/index.js
@@ -0,0 +1,16 @@
+export const Protocols = Object.freeze({
+  DEFAULT: "http://",
+  SOCKET: "ws://",
+});
+
+export const Hosts = Object.freeze({
+  CLIENT: "localhost:3000/",
+  SERVER: "localhost:8080/",
+  SERVER_API: "localhost:8080/api/",
+  SERVER_SOCKET: "localhost:8080/socket/",
+});
+
+export const RoleTypes = Object.freeze({
+  USER: "ROLE_USER",
+  ADMIN: "ROLE_ADMIN",
+});
