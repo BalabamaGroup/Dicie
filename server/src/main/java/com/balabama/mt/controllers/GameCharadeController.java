5529d5b4b59c3a0a09e28287bd32142280b9af5b
SharunovEvgeny <jenya.sharunov@gmail.com>
2022-10-29 20:58:26 +0300

    Change word path variable  to request body

--- server/src/main/java/com/balabama/mt/controllers/GameCharadeController.java
+++ server/src/main/java/com/balabama/mt/controllers/GameCharadeController.java
@@ -1,10 +1,12 @@
 package com.balabama.mt.controllers;
 
 import com.balabama.mt.converters.RoomDtoConverter;
+import com.balabama.mt.dtos.StringDto;
 import com.balabama.mt.dtos.room.RoomDto;
 import com.balabama.mt.services.GameCharadeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,14 +23,14 @@ public class GameCharadeController {
     private final WebSocketHandler webSocketHandler;
 
 
-    @PostMapping("/set_word/{userId}")
-    public void setWord(@PathVariable Long userId, @RequestBody String word) {
-        webSocketHandler.sendRoomMessage(converter.convertRoom(service.setWord(userId, word)));
+    @PostMapping(value = "/set_word/{userId}")
+    public void setWord(@PathVariable Long userId, @RequestBody StringDto word) {
+        webSocketHandler.sendRoomMessage(converter.convertRoom(service.setWord(userId, word.getWord())));
     }
 
     @PostMapping("/check_word")
-    public void checkWord(@RequestBody String word) {
-        webSocketHandler.sendRoomMessage(converter.convertRoom(service.checkWord(word)));
+    public void checkWord(@RequestBody StringDto word) {
+        webSocketHandler.sendRoomMessage(converter.convertRoom(service.checkWord(word.getWord())));
     }
 
 }
