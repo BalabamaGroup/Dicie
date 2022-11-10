028ea220b84e73a0ac5dadf71f4eb05d1edcb184
SharunovEvgeny <jenya.sharunov@gmail.com>
2022-11-11 02:35:52 +0300

    Add disconnect

--- server/src/main/java/com/balabama/mt/service_implementation/RoomServiceImpl.java
+++ server/src/main/java/com/balabama/mt/service_implementation/RoomServiceImpl.java
@@ -49,6 +49,9 @@ public class RoomServiceImpl implements RoomService {
     public Room disconnect(UUID id) {
         Room room = getById(id);
         room.disconnect(userService.getCurrent());
+        if (room.getRoomData().checkFinish()){
+            return finish(room.getId());
+        }
         if (room.getUsers().size() == 1) {
             finish(room.getId());
         }
