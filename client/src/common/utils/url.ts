780170dea2db85e8ee236a89060ae96ae68090da
Mushka Nikita <nikitamushka@mail.ru>
2023-01-24 20:48:14 +0300

    changed url hotname

--- client/src/common/utils/url.ts
+++ client/src/common/utils/url.ts
@@ -1,9 +1,12 @@
 import { Hosts, Protocols } from '@/common/constants';
 
 export const apiUrl = () => {
-  return Protocols.DEFAULT + Hosts.SERVER_API;
+  console.log(location);
+  console.log('Api URL', Protocols.DEFAULT + window.location.hostname);
+  return Protocols.DEFAULT + window.location.hostname + ':8080' + '/api/';
 };
 
 export const socketUrl = () => {
-  return Protocols.SOCKET + Hosts.SERVER_SOCKET;
+  console.log('Socket URL', Protocols.DEFAULT + window.location.hostname);
+  return Protocols.SOCKET + window.location.hostname + ':8080' + '/socket/';
 };
