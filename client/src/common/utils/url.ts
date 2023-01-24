38e58d54b95412c8a61a8407585cee0fc63cdbe8
Mushka Nikita <nikitamushka@mail.ru>
2023-01-24 22:38:41 +0300

    cleande up code

--- client/src/common/utils/url.ts
+++ client/src/common/utils/url.ts
@@ -1,19 +1,9 @@
 import { Hosts, Protocols } from '@/common/constants';
 
 export const apiUrl = () => {
-  let hostname = window.location.hostname;
-  if (hostname !== 'localhost') hostname = '164.92.151.90';
-  console.log(window.location.hostname, hostname);
-
-  console.log('Api URL', Protocols.DEFAULT + hostname);
   return Protocols.DEFAULT + window.location.hostname + ':8080' + '/api/';
 };
 
 export const socketUrl = () => {
-  let hostname = window.location.hostname;
-  if (hostname !== 'localhost') hostname = '164.92.151.90';
-  console.log(window.location.hostname, hostname);
-
-  console.log('Socket URL', Protocols.DEFAULT + window.location.hostname);
   return Protocols.SOCKET + window.location.hostname + ':8080' + '/socket/';
 };
