2c6a2f7a9184b9275b4acae9c0e12b08b15eed1a
Mushka Nikita <nikitamushka@mail.ru>
2023-01-24 21:11:16 +0300

    hostname fix

--- client/src/common/utils/url.ts
+++ client/src/common/utils/url.ts
@@ -2,7 +2,7 @@ import { Hosts, Protocols } from '@/common/constants';
 
 export const apiUrl = () => {
   let hostname = window.location.hostname;
-  if (hostname === 'localhost') hostname = '164.92.151.90';
+  if (hostname !== 'localhost') hostname = '164.92.151.90';
 
   console.log('Api URL', Protocols.DEFAULT + window.location.hostname);
   return Protocols.DEFAULT + window.location.hostname + ':8080' + '/api/';
@@ -10,7 +10,7 @@ export const apiUrl = () => {
 
 export const socketUrl = () => {
   let hostname = window.location.hostname;
-  if (hostname === 'localhost') hostname = '164.92.151.90';
+  if (hostname !== 'localhost') hostname = '164.92.151.90';
 
   console.log('Socket URL', Protocols.DEFAULT + window.location.hostname);
   return Protocols.SOCKET + hostname + ':8080' + '/socket/';
