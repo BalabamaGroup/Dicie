e0cfa6d58098847c5f19dd54e265d06e7f993b80
SharunovEvgeny <jenya.sharunov@gmail.com>
2023-02-25 22:51:40 +0300

    Add chat

--- server/src/main/resources/db/changelog/changeset/DICIE-2.sql
+++ server/src/main/resources/db/changelog/changeset/DICIE-2.sql
@@ -3,8 +3,8 @@
 --changeset evgeniy.sharunov:DICIE-2
 CREATE TABLE `chat`
 (
-    room_id    varchar(36) not null
+    id    varchar(36) not null
         primary key,
-    `log` TEXT(65535) NULL DEFAULT NULL;
+    `log` TEXT(65535) NULL DEFAULT NULL
 );
 --rollback DROP TABLE `chat`;
