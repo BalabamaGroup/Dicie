0cae318a2c32eb11c251d65d42df79c38cb27280
Mushka Nikita <nikitamushka@mail.ru>
2023-04-17 16:24:18 +0300

    removed ports

--- client/src/common/utils/url.ts
+++ client/src/common/utils/url.ts
@@ -1,11 +1,14 @@
 import { Ports, Protocols } from '@/common/constants';
 
 export const apiUrl = () => {
-  return Protocols.DEFAULT + window.location.hostname + Ports.DEFAULT + '/api/';
+  return Protocols.DEFAULT + window.location.hostname + '/api/';
+  // return Protocols.DEFAULT + window.location.hostname + Ports.DEFAULT + '/api/';
 };
 
 export const roomSocketUrl =
-  Protocols.SOCKET + window.location.hostname + Ports.DEFAULT + '/socket';
+  Protocols.SOCKET + window.location.hostname + '/socket';
+// Protocols.SOCKET + window.location.hostname + Ports.DEFAULT + '/socket';
 
 export const chatSocketUrl =
-  Protocols.SOCKET + window.location.hostname + Ports.DEFAULT + '/chat-socket';
+  Protocols.SOCKET + window.location.hostname + '/chat-socket';
+// Protocols.SOCKET + window.location.hostname + Ports.DEFAULT + '/chat-socket';
