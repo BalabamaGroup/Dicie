e6378e35d9009ade1061b9b74749236685079685
Mushka Nikita <nikitamushka@mail.ru>
2023-04-21 02:45:21 +0300

    optimization tweaks

--- client/src/pages/Home/ContentCards/HomeContentCard.styled.ts
+++ client/src/pages/Home/ContentCards/HomeContentCard.styled.ts
@@ -25,9 +25,9 @@ const HomeContentCard = styled.div<{
     position: absolute;
     height: 48px;
     width: 48px;
-    transition: opacity 0.3s ease-in-out;
-    opacity: ${({ isDefault, isSelected }) =>
-      !isDefault && !isSelected ? '1' : '0'};
+    /* transition: opacity 0.3s ease-in-out; */
+    display: ${({ isDefault, isSelected }) =>
+      !isDefault && !isSelected ? 'block' : 'none'};
     svg {
       height: 48px;
       width: 48px;
@@ -38,16 +38,15 @@ const HomeContentCard = styled.div<{
     height: 100%;
     width: 100%;
     text-align: center;
-    transition: opacity 0.2s ease-in-out;
-    opacity: ${({ isDefault, isSelected }) =>
-      isDefault && !isSelected ? '1' : '0'};
+    /* transition: opacity 0.2s ease-in-out; */
+    display: ${({ isDefault, isSelected }) =>
+      isDefault && !isSelected ? 'flex' : 'none'};
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     box-sizing: border-box;
     user-select: none;
-    display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
@@ -72,10 +71,12 @@ const HomeContentCard = styled.div<{
   .on-selected {
     height: 100%;
     width: 100%;
+
+    will-change: opacity;
+    transition: opacity 0.2s ease-in-out, transform 0.2s ease-in-out;
     opacity: ${({ isDefault, isSelected }) =>
       !isDefault && isSelected ? '1' : '0'};
 
-    transition: opacity 0.2s ease-in-out, transform 0.2s ease-in-out;
     transform: translateX(-50%) translateY(calc(-50% - 0.5px));
     /* transform: translate(-50%, -50%)
       ${({ isDefault, isSelected }) =>
