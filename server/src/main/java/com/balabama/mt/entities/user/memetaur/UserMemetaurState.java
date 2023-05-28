61600c3d6e42bee43dbf81881fe1092af68e7497
SharunovEvgeny <jenya.sharunov@gmail.com>
2023-05-28 20:16:14 +0300

    Add isGoing

--- server/src/main/java/com/balabama/mt/entities/user/memetaur/UserMemetaurState.java
+++ server/src/main/java/com/balabama/mt/entities/user/memetaur/UserMemetaurState.java
@@ -3,8 +3,11 @@ package com.balabama.mt.entities.user.memetaur;
 import com.balabama.mt.dtos.user.memetaur.UserMemetaurStateDto;
 import com.balabama.mt.entities.user.User;
 import com.balabama.mt.entities.user.UserState;
+import com.balabama.mt.entities.user.guessBoo.UserGuessBooState;
+import com.balabama.mt.exceptions.MTException;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,7 @@ public class UserMemetaurState extends UserState {
     private String gif;
     private String votedGif;
     private Integer countWin = 0;
+    private Boolean isGoing = false;
 
     public UserMemetaurState(User user) {
         super(user);
@@ -40,9 +44,9 @@ public class UserMemetaurState extends UserState {
     }
 
     public void checkTurn() {
-//        if (!isGoing) {
-//            throw new MTException(HttpStatus.BAD_REQUEST, "It's not your turn now");
-//        }
+        if (!isGoing) {
+            throw new MTException(HttpStatus.BAD_REQUEST, "It's not your turn now");
+        }
     }
 
     public static List<User> addUserStates(List<User> users) {
@@ -51,6 +55,7 @@ public class UserMemetaurState extends UserState {
             user.setUserState(new UserMemetaurState(user));
             userList.add(user);
         }
+        ((UserMemetaurState) userList.get(0).getUserState()).setIsGoing(true);
         return userList;
     }
 
